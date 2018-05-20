1cab7f7f70b89438b2aa9e43b2ae74929ba247fd
PattarapornH <pataraporn.han@gmail.com>
2018-05-20 12:48:55 +0700

    fixed length button

--- SelectShip/core/src/com/mygdx/game/World.java
+++ SelectShip/core/src/com/mygdx/game/World.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.Select;
 
@@ -18,6 +19,7 @@ public class World {
     public float deltaTime;
     public float totalTime = 0;
     public float startTime;
+//    Sound music;
 
     World(SelectShip SelectShip) {
         this.SelectShip = SelectShip;
@@ -26,10 +28,13 @@ public class World {
         shipvik = new shipViking(1250,700,this);
         correct = new Correct(1250,700,this);
         wrong = new Wrong(1250,700,this);
+//        music = Gdx.audio.newSound(Gdx.files.internal("GamingBG.mp3"));
     }
 
     public void update(float delta){
         Timer();
+        System.out.println(Gdx.input.getX() + " " + Gdx.input.getY());
+//        music.play();
 //        if((int)totalTime >= 3){
 //            gameState = -1;
 //        }
@@ -47,15 +52,15 @@ public class World {
 
 
     public boolean pointOnShipcar(int x,int y){
-        return (x >= 347 && x <= 629) && (y >= 70 && y <= 317);
+        return (x >= 516 && x <= 938) && (y >= 97 && y <= 446);
     }
 
     public boolean pointOnShipchi(int x,int y){
-        return (x >= 110 && x <= 426) && (y >= 368 && y <= 621);
+        return (x >= 163 && x <= 633) && (y >= 535 && y <= 857);
     }
 
     public boolean pointOnShipvik(int x,int y){
-        return (x >= 484 && x <= 808) && (y >= 369 && y <= 616);
+        return (x >= 731 && x <= 1198) && (y >= 514 && y <= 853);
     }
 
 
